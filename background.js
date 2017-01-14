7039c150626bbc86e98165363585dc5b5697943e
Justin Foley <justin@modweb.io>
2017-01-13 19:13:55 -0500

    toggle off on icon click

--- background.js
+++ background.js
@@ -1,3 +1,20 @@
+const trimHostname = function (hostname) {
+  return hostname.replace(/^www\./,'');
+};
+
+const getCSSURL = function (hostname) {
+  return "https://rawgit.com/weblintio/core-rules/master/rules/"+hostname+".css";
+};
+
+const removeStyle = {
+  code: `
+    var styleElement = document.getElementById('weblint-style');
+    if (!!styleElement) {
+      document.head.removeChild(styleElement);
+    }
+    `,
+}
+
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   if (changeInfo.status != 'loading') {
     return;
@@ -13,26 +30,23 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
 
-      const experiment = {
+      const addStyle = {
         code: `
           var styleElement = document.createElement('style');
           styleElement.id = 'weblint-style';
           styleElement.appendChild(document.createTextNode("${this.response.replace(/(\r\n|\n|\r)/gm,"")}"));
           document.head.appendChild(styleElement);
+          document.weblint = true;
           `,
         runAt: 'document_start'
       }
 
-      chrome.tabs.executeScript(tabId, experiment);
+      chrome.tabs.executeScript(tabId, addStyle);
     }
   };
   xhttp.send();
 });
 
-const trimHostname = function (hostname) {
-  return hostname.replace(/^www\./,'');
-};
-
-const getCSSURL = function (hostname) {
-  return "https://rawgit.com/weblintio/core-rules/master/rules/"+hostname+".css";
-};
+chrome.browserAction.onClicked.addListener(function(tab) {
+  chrome.tabs.executeScript(tab.tabId, removeStyle);
+});
